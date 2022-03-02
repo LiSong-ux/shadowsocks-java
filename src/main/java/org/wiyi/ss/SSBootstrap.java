0836e157a86f5a3f868b38199efb848a56290063
LiSong-ux <song.li@mycool.tv>
2022-03-02 19:52:36 +0800

    SSBootstrap.java代码解读

--- src/main/java/org/wiyi/ss/SSBootstrap.java
+++ src/main/java/org/wiyi/ss/SSBootstrap.java
@@ -7,10 +7,11 @@ import org.wiyi.ss.core.config.SSConfigValidator;
 import org.wiyi.ss.utils.SSCommandLineUtils;
 import org.wiyi.ss.utils.VersionUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 
+/**
+ * 这是整个工程的启动类，运行jar包时添加的参数由该类接收
+ */
 public class SSBootstrap {
     public static final String SS_LOCAL = "sslocal";
     public static final String SS_SERVER = "ssserver";
@@ -30,24 +31,27 @@ public class SSBootstrap {
         boolean isSSLocal = command.equalsIgnoreCase(SS_LOCAL);
 
         CommandLineParser parser = new DefaultParser();
+        // 获取应用的启动参数对象Options
         Options options = isSSLocal ? SSCommandLineUtils.ssLocalOptions() : SSCommandLineUtils.ssServerOptions();
 
         try {
-            CommandLine cmd = parser.parse(options,bootArgs);
+            CommandLine cmd = parser.parse(options, bootArgs);
             if (cmd.hasOption("h") || cmd.getArgList().isEmpty()) {
-                printHelper(options,command);
+                // 如果启动命令中有“h”选项或者参数为空，则打印所有的命令选项并退出
+                printHelper(options, command);
                 System.exit(0);
             } else if (cmd.hasOption("v")) {
+                // 如果启动命令中有“v”选项，则打印应用版本并退出
                 System.out.println(VersionUtils.getVersion());
                 System.exit(0);
             }
 
             SSConfig config = SSConfigFactory.loadFromCommandLine(cmd);
-            SSConfigValidator.validate(config,isSSLocal);
+            SSConfigValidator.validate(config, isSSLocal);
             server = isSSLocal ? new SSLocal(config) : new SSServer(config);
         } catch (Exception e) {
             System.err.println(e.getMessage());
-            printHelper(options,command);
+            printHelper(options, command);
             System.exit(1);
         }
 
@@ -62,12 +66,18 @@ public class SSBootstrap {
         System.exit(0);
     }
 
-
+    /**
+     * 判断是不是合法的命令行，只有 sslocal或ssserver开头的命令才是合法的
+     *
+     * @param argv 用户输入的命令行
+     * @return 是否合法
+     */
     private boolean isValidCommand(String argv) {
         return SS_LOCAL.equalsIgnoreCase(argv) || SS_SERVER.equalsIgnoreCase(argv);
     }
 
-    private void printHelper(Options all,String command) {
+    // 打印所有的命令行选项
+    private void printHelper(Options all, String command) {
         Options options = new Options();
         Options flags = new Options();
         for (Option option : all.getOptions()) {
@@ -81,14 +91,14 @@ public class SSBootstrap {
         HelpFormatter formatter = new HelpFormatter();
         PrintWriter writer = new PrintWriter(System.out);
 
-        formatter.printUsage(writer,formatter.getWidth(),command + " [FLAGS] [OPTIONS]");
+        formatter.printUsage(writer, formatter.getWidth(), command + " [FLAGS] [OPTIONS]");
         int with = formatter.getWidth() + 35;
 
         writer.println("\nflags:");
-        formatter.printOptions(writer,with,flags,formatter.getLeftPadding(),formatter.getDescPadding());
+        formatter.printOptions(writer, with, flags, formatter.getLeftPadding(), formatter.getDescPadding());
 
         writer.println("\noptions:");
-        formatter.printOptions(writer,with,options,formatter.getLeftPadding(),formatter.getDescPadding());
+        formatter.printOptions(writer, with, options, formatter.getLeftPadding(), formatter.getDescPadding());
         writer.flush();
     }
 }
